6c9be2810581c5526e55fc5b18936d5557aae252
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-06-14 12:03:18 +0200

    add select for portfolio categories

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
+// nav bar
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
+
+// portfolio projects
+const selectPortfolioCategories = document.querySelector(
+  "#portfolios-categories"
+);
+
+function getPortfoliosCategories() {
+  const projects = getLocalStorageData("projects_github_portfolio");
+  return Object.keys(projects);
+}
+
+function fillSelectPortfolios(portfoliosCategories) {
+  for (const cat of portfoliosCategories) {
+    selectPortfolioCategories.innerHTML += `<option value="${cat}">${cat}</option>`;
+  }
+}
+
+const saveLocalStorage = (data, key) => {
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
+const getLocalStorageData = (key) => {
+  return JSON.parse(localStorage.getItem(key)) || [];
+};
+
+selectPortfolioCategories.addEventListener("change", function () {
+  displayProjectsPortfolio(selectPortfolioCategories.value);
+});
+
 fetch("./projects.json")
   .then(function (res) {
     if (res.ok) {
       return res.json();
     }
   })
-  .then(function (value) {
-    displayAllProjects(value);
+  .then(function (data) {
+    saveLocalStorage(data, "projects_github_portfolio");
+    const portfoliosCategories = getPortfoliosCategories();
+    fillSelectPortfolios(portfoliosCategories);
+    displayProjectsPortfolio(null);
+    allProjects = getLocalStorageData("projects_github_portfolio");
   })
   .catch(function (err) {
     // Une erreur est survenue
@@ -19,22 +59,28 @@ function displayOneProject(project) {
                   <p class='techs'>${project.techs.join("|")}</p>
                   <a href="${
                     project.github_link
-                  }" target="_blank" class="link-code">See sourcecode</a>
+                  }" target="_blank" class="link-code">See source code</a>
                 </div>
               </div>`;
 
   return html;
 }
 
-function displayAllProjects(data) {
-  const htmlProjects = data.map(project => displayOneProject(project));
+function displayProjectsPortfolio(categoryName = null) {
+  let projects = getLocalStorageData("projects_github_portfolio");
+  const portfoliosCategories = getPortfoliosCategories();
+
+  projects = categoryName
+    ? projects[categoryName]
+    : projects[portfoliosCategories[0]];
+
+  const htmlProjects = projects.map((project) => displayOneProject(project));
   let divProjects = document.getElementById("grid");
   divProjects.innerHTML = htmlProjects;
 }
 
-const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".links");
-
-navToggle.addEventListener("click", function () {
-  links.classList.toggle("show-links");
-});
+function displayAllProjects(data) {
+  const htmlProjects = data.map((project) => displayOneProject(project));
+  let divProjects = document.getElementById("grid");
+  divProjects.innerHTML = htmlProjects;
+}
